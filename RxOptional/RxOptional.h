f92a09e81639c27a58b0526bf7ae1de9f6102685
Sam Ritchie <sam@codesplice.com.au>
2016-04-27 20:39:51 +0800

    Added Carthage support

---
+++ RxOptional/RxOptional.h
@@ -0,0 +1,19 @@
+//
+//  RxOptional.h
+//  RxOptional
+//
+//  Created by Sam Ritchie on 27/04/2016.
+//  Copyright © 2016 RxSwiftCommunity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxOptional.
+FOUNDATION_EXPORT double RxOptionalVersionNumber;
+
+//! Project version string for RxOptional.
+FOUNDATION_EXPORT const unsigned char RxOptionalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxOptional/PublicHeader.h>
+
+
